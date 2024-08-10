83e5339a0e5e6ab835fd1112df998a9b810e5b67
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-08-09 22:56:52 -0300

    inicial commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/doctors', require('./routes/doctors'));
+app.use('/api/appointments', require('./routes/appointments'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
