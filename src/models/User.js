7102dd6ad04fa3995b2f78a3a1211f5d3356ea3e
Willian R <158328639+xXWilliaN12Xx@users.noreply.github.com>
2024-08-11 22:14:27 -0300

    Day 44

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+// models/User.js
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['patient', 'doctor', 'admin'],
+        default: 'patient',
+    },
+});
+
+// Hash the password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model('User', UserSchema)
